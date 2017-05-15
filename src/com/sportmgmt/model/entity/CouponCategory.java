5380790f5e1f5da9fc51fd718b0debfe1978e6b5
deveshsoftwaregroup <deveshsoftwaregroup@gmail.com>
2017-05-15 17:36:36 +0530

    Coupon work

---
+++ src/com/sportmgmt/model/entity/CouponCategory.java
@@ -0,0 +1,132 @@
+package com.sportmgmt.model.entity;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+@Entity
+@Table(name="COUPON_CATEGORY")
+public class CouponCategory {
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name="COUPON_CATEGORY_ID")
+	private Integer couponCategoryId;
+	
+	@Column(name="NAME")
+	private String name;
+	
+	@Column(name="DESCRIPTION")
+	private String description;
+	
+	@OneToMany(cascade=CascadeType.ALL,fetch=FetchType.EAGER)
+	@JoinColumn(name="COUPON_CATEGORY_ID",insertable=true, updatable=true,nullable=true,unique=true)
+	private List<Coupon> couponList;
+	
+	@Column(name="TYPE")
+	private String type;
+	
+	@Column(name="IS_ACTIVE")
+	private String isActive;
+	
+	@Column(name="STATUS")
+	private String status;
+	
+	@Column(name="MIN_PONT")
+	private Integer minPoint;
+	
+	@Column(name="MAX_PONT")
+	private Integer maxPoint;
+	
+	@Column(name="FIELD1")
+	private String field1;
+
+	public Integer getCouponCategoryId() {
+		return couponCategoryId;
+	}
+
+	public void setCouponCategoryId(Integer couponCategoryId) {
+		this.couponCategoryId = couponCategoryId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<Coupon> getCouponList() {
+		return couponList;
+	}
+
+	public void setCouponList(List<Coupon> couponList) {
+		this.couponList = couponList;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(String isActive) {
+		this.isActive = isActive;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Integer getMinPoint() {
+		return minPoint;
+	}
+
+	public void setMinPoint(Integer minPoint) {
+		this.minPoint = minPoint;
+	}
+
+	public Integer getMaxPoint() {
+		return maxPoint;
+	}
+
+	public void setMaxPoint(Integer maxPoint) {
+		this.maxPoint = maxPoint;
+	}
+
+	public String getField1() {
+		return field1;
+	}
+
+	public void setField1(String field1) {
+		this.field1 = field1;
+	}
+	
+}
