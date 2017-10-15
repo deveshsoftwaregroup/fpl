2ea2fbcad7673bdffc5260a735eca4fa0c77154f
deveshsoftwaregroup <deveshsoftwaregroup@gmail.com>
2017-10-15 19:02:19 +0530

    environment files

--- src/com/sportmgmt/utility/common/PropertyFileUtility.java
+++ src/com/sportmgmt/utility/common/PropertyFileUtility.java
@@ -6,6 +6,8 @@ import com.sportmgmt.utility.constrant.SportConstrant;
 
 public class PropertyFileUtility {
 	public ResourceBundle getEnvProperty() throws Exception {
-		return ResourceBundle.getBundle(SportConstrant.ENV_FILE_NAME);
+		return ResourceBundle.getBundle(SportConstrant.ENV_FILE_NAME_LOCAL);
+		//return ResourceBundle.getBundle(SportConstrant.ENV_FILE_NAME_STAGE);
+		//return ResourceBundle.getBundle(SportConstrant.ENV_FILE_NAME_LIVE);
 	}		
 }
